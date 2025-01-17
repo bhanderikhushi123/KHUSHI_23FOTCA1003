1f8f3d506c62ac722e18747218ed65c82219ffab
01 <01@KHUSHI-BHANDERI>
2025-01-17 01:09:26 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHUSHI_23FOTCA1003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
