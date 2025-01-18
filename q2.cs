ff4d0d539ee74e6cd77c4660b026dbf151f2cd59
bhanderikhushi866 <bhanderikhushi866@gmail.com>
2025-01-18 00:21:12 -0800

    commit

---
+++ q2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHUSHI_23FOTCA1003
+{
+    internal class q2
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 0; i<=4; i++)
+            {
+                for(int j = 0; j<=4; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ q3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHUSHI_23FOTCA1003
+{
+    internal class q3
+    {
+        public static void Main(String[] args)
+        { 
+            for(int i = 1; i <=5; i++) 
+            {
+               for (int j = 1; j <=i; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        } 
+    }
+}
